e5e92d6b97c4020adf0d50ed4ad4f225922b4865
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-03-11 17:50:27 +0530

    added search functionality

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -7,28 +7,42 @@ export const Products = () => {
 const [loading, setLoading] = React.useState(false);
 const [query, setQuery] = React.useState("random");
   const divRef = React.useRef();
-  React.useEffect(() => {setData([])},[query])
+    React.useEffect(() => {
+    setLoading(true);
+        fetch(
+            `https://api.pexels.com/v1/search?query=${query}&page=1&per_page=25`,
+            {
+              method: "GET",
+              headers: {
+                Authorization:
+                  "563492ad6f9170000100000180a152de2b3a48579c71ec5794cc6bb7"
+              }
+            }
+          )
+            .then((response) => response.json())
+            .then((response) => {
+              setData([...response.photos]);
+              setLoading(false);
+            });
+  },[query])
   React.useEffect(() => {
     setLoading(true);
-    getData();
-  }, [query,page]);
-  function getData() {
     fetch(
-      `https://api.pexels.com/v1/search?query=${query}&page=${page}&per_page=25`,
-      {
-        method: "GET",
-        headers: {
-          Authorization:
-            "563492ad6f9170000100000180a152de2b3a48579c71ec5794cc6bb7"
+        `https://api.pexels.com/v1/search?query=${query}&page=${page}&per_page=25`,
+        {
+          method: "GET",
+          headers: {
+            Authorization:
+              "563492ad6f9170000100000180a152de2b3a48579c71ec5794cc6bb7"
+          }
         }
-      }
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        setData([...data, ...response.photos]);
-        setLoading(false);
-      });
-  }
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setData([...data, ...response.photos]);
+          setLoading(false);
+        });
+  }, [page]);
   const scroll = () => {
     if (
       divRef.current.scrollTop + divRef.current.clientHeight >
